7113984aabb9c818b6c48bbae45d5de8bb7fc075
Pslydhh <luouyuyou@gmail.com>
2017-03-29 21:33:37 +0800

    提交我的最大可开辟数组文件
    	osososososososososososososososososos

---
+++ PslyMaxAllocatedArraySize/src/main/java/com/kiwi/Hello2/PslyMaxAllocatedArraySize.java
@@ -0,0 +1,23 @@
+package com.kiwi.Hello2;
+
+/**
+ * Hello world!
+ *
+ */
+public class PslyMaxAllocatedArraySize 
+{
+	 public static void  main(String[] arg){
+	     
+		 int[] my = new int[353107965];
+		int[] you = new int[8385589];
+
+		
+		
+	        try {
+				Thread.sleep(2000L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+}
---
+++ PslyMaxAllocatedArraySize/src/test/java/com/kiwi/Hello2/AppTest.java
@@ -0,0 +1,20 @@
+package com.kiwi.Hello2;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+	  	@Test
+	    public void testOne(){
+	        PslyMaxAllocatedArraySize one = new PslyMaxAllocatedArraySize();
+	//        String result = one.say("AAA");
+	//        Assert.assertEquals("Hello Maven AAA",result);
+	    }
+}
